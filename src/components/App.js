b4a4c5de59d01eadb2732843003acc41412e98e1
nali-kang <softnali92@gmail.com>
2024-09-04 17:59:38 +0900

    useEffect, 페이지네이션 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import ReviewList from "./ReviewList";
-import mockItems from "../mock.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getReviews } from "../api";
+
+const LIMIT = 6;
 
 function App() {
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleBestClick = () => setOrder("rating");
@@ -16,12 +20,39 @@ function App() {
     setItems(nextItems);
   };
 
+  const handleLoad = async (options) => {
+    const { reviews, paging } = await getReviews(options);
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems([...items, ...reviews]);
+    }
+    setOffset(options.offset + reviews.length);
+    setHasNext(paging.hasNext);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({ order, offset, limit: LIMIT });
+  };
+
+  const handleReset = () => {
+    handleLoad({ order, offset: 0, limit: LIMIT });
+  };
+
+  useEffect(() => {
+    handleLoad({ order, offset: 0, limit: LIMIT });
+  }, [order]);
+
   return (
     <div>
       <button onClick={handleBestClick}>베스트</button>
       <button onClick={handleNewClick}>최신순</button>
 
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      {hasNext && <button onClick={handleLoadMore}>더보기</button>}
+      <button onClick={handleReset} disabled={offset === 6}>
+        초기화
+      </button>
     </div>
   );
 }
